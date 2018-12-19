b43eb0e0ff8052c9b9b30017badfe406847760de
wintersing <15779819842@163.com>
2018-12-19 23:12:00 +0800

    酒店接口

---
+++ src/main/java/com/lt/journey/util/HotelInfo.java
@@ -0,0 +1,12 @@
+package com.lt.journey.util;
+
+import org.apache.http.client.methods.HttpGet;
+
+public class HotelInfo {
+
+//	private static String = "http://open.zhuna.cn/api/gateway.php?method=search&必选参数%7C可选参数";
+	
+	public void getHotelInfo() {
+		HttpGet httpGet = new HttpGet();
+	}
+}
