0b9752c1f3a92146e69b365a7692fe61f6da5d76
wintersing <15779819842@163.com>
2018-12-20 21:56:11 +0800

    酒店列表查询接口调用成功

---
+++ src/main/java/com/lt/journey/model/HotelParam.java
@@ -0,0 +1,96 @@
+package com.lt.journey.model;
+
+public class HotelParam {
+	private Double lon;
+	private Double lat;
+	private String distance;
+	private String kw;
+	private String pageToken;
+	private String city;
+	private String level;
+	private String brandName;
+	private String address;
+	private String businessDistrict;
+	private String sortByField;
+	private String appCode;
+	public Double getLon() {
+		return lon;
+	}
+	public void setLon(Double lon) {
+		this.lon = lon;
+	}
+	public Double getLat() {
+		return lat;
+	}
+	public void setLat(Double lat) {
+		this.lat = lat;
+	}
+	public String getDistance() {
+		return distance;
+	}
+	public void setDistance(String distance) {
+		this.distance = distance;
+	}
+	public String getKw() {
+		return kw;
+	}
+	public void setKw(String kw) {
+		this.kw = kw;
+	}
+	public String getPageToken() {
+		return pageToken;
+	}
+	public void setPageToken(String pageToken) {
+		this.pageToken = pageToken;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getLevel() {
+		return level;
+	}
+	public void setLevel(String level) {
+		this.level = level;
+	}
+	public String getBrandName() {
+		return brandName;
+	}
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getBusinessDistrict() {
+		return businessDistrict;
+	}
+	public void setBusinessDistrict(String businessDistrict) {
+		this.businessDistrict = businessDistrict;
+	}
+	public String getSortByField() {
+		return sortByField;
+	}
+	public void setSortByField(String sortByField) {
+		this.sortByField = sortByField;
+	}
+	public String getAppCode() {
+		return appCode;
+	}
+	public void setAppCode(String appCode) {
+		this.appCode = appCode;
+	}
+	@Override
+	public String toString() {
+		return "HotelParam [lon=" + lon + ", lat=" + lat + ", distance=" + distance + ", kw=" + kw + ", pageToken="
+				+ pageToken + ", city=" + city + ", level=" + level + ", brandName=" + brandName + ", address="
+				+ address + ", businessDistrict=" + businessDistrict + ", sortByField=" + sortByField + ", appCode="
+				+ appCode + "]";
+	}
+	
+}
--- src/main/java/com/lt/journey/util/HotelInfo.java
+++ src/main/java/com/lt/journey/util/HotelInfo.java
@@ -1,12 +1,35 @@
 package com.lt.journey.util;
 
-import org.apache.http.client.methods.HttpGet;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.lt.journey.model.HotelParam;
+
+import commons.utils.CommonsUtils;
 
 public class HotelInfo {
 
-	private static String URL = "http://open.zhuna.cn/api/gateway.php?method=search&必选参数%7C可选参数";
+	private static String url = "http://api01.idataapi.cn:8000/hotel/idataapi";
+	private static String apikey = "zq71QLetvWkIEefHcNCV7MTq5NrVGPrj8A0rLYoo3jSEv5es8OGd7c3zAQ0o592p";
+
+	public static void getHotelInfo(HotelParam hotelParam) {
+		StringBuffer param = new StringBuffer();
+		param.append("apikey=" + apikey);
+		Map<String, Object> map = CommonsUtils.beantoMap(hotelParam);
+		for (String key : map.keySet()) {
+			if (map.get(key) != "" && map.get(key) != null) {
+				param.append("&" + key + "=" + map.get(key));
+			}
+		}
+		String ret = HttpRequest.sendGet(url, param.toString());
+		System.out.println(ret);
+	}
 	
-	public void getHotelInfo() {
-		HttpGet httpGet = new HttpGet(URL);
+	@Test
+	public void name() {
+		HotelParam hotelParam = new HotelParam();
+		hotelParam.setCity("景德镇");
+		getHotelInfo(hotelParam);
 	}
 }
