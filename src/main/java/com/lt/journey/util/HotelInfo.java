98b54f33fabd5093b2f9df8d8cc688cb8a242896
wintersing <15779819842@163.com>
2018-12-20 10:34:25 +0800

    123

--- src/main/java/com/lt/journey/util/HotelInfo.java
+++ src/main/java/com/lt/journey/util/HotelInfo.java
@@ -4,9 +4,9 @@ import org.apache.http.client.methods.HttpGet;
 
 public class HotelInfo {
 
-//	private static String = "http://open.zhuna.cn/api/gateway.php?method=search&必选参数%7C可选参数";
+	private static String URL = "http://open.zhuna.cn/api/gateway.php?method=search&必选参数%7C可选参数";
 	
 	public void getHotelInfo() {
-		HttpGet httpGet = new HttpGet();
+		HttpGet httpGet = new HttpGet(URL);
 	}
 }
